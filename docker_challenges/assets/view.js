2163775849a7c9a24142be0ef26809db405e9244
Kalnyia <18225832+CorentinFelten@users.noreply.github.com>
2025-03-27 19:04:36 -0400

    Fixing container check

--- docker_challenges/assets/view.js
+++ docker_challenges/assets/view.js
@@ -48,60 +48,68 @@ CTFd._internal.challenge.submit = function(preview) {
     })
 };
 
+
 function get_docker_status(container) {
-    CTFd.fetch("/api/v1/docker_status")
+    return CTFd.fetch("/api/v1/docker_status")
         .then((data) => {
-            data.json().then((result) => {
-                var hostname = data.url.split('//')[1].split('/')[0];
+            return data.json().then((result) => {
+                const hostname = data.url.split('//')[1].split('/')[0];
+                let containerFound = false;
+
                 CTFd.lib.$.each(result['data'], function(i, item) {
                     if (item.docker_image === container) {
-                        var ports = String(item.ports).split(',');
-                        var data = '';
+                        containerFound = true;
+                        const ports = String(item.ports).split(',');
+                        let data = '';
                         CTFd.lib.$.each(ports, function(x, port) {
                             port = String(port).split('/')[0];
                             data = data + `<a href='http://${hostname}:${port}' target='_blank'>http://${hostname}:${port}</a><br />`;
-                        })
+                        });
                         CTFd.lib.$('#docker_container').html('<pre>Instance available at:<br />' + data + '<div class="mt-2" id="' + String(item.instance_id).substring(0,10) + '_revert_container"></div>');
-                        var countDownDate = new Date(parseInt(item.revert_time) * 1000).getTime();
-                        var x = setInterval(function() {
-                            var now = new Date().getTime();
-                            var distance = countDownDate - now;
-                            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                        const countDownDate = new Date(parseInt(item.revert_time) * 1000).getTime();
+                        const x = setInterval(function() {
+                            const now = new Date().getTime();
+                            const distance = countDownDate - now;
+                            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
                             if (seconds < 10) {
-                                seconds = "0" + seconds
+                                seconds = "0" + seconds;
                             }
                             CTFd.lib.$("#" + String(item.instance_id).substring(0,10) + "_revert_container").html('Next Revert Available in ' + minutes + ':' + seconds);
                             if (distance < 0) {
                                 clearInterval(x);
-                                CTFd.lib.$("#" + String(item.instance_id).substring(0,10) + "_revert_container").html('<a onclick="start_container(\'' + item.docker_image + '\',true);" class=\'btn btn-dark\'><small style=\'color:white;\'><i class="fas fa-redo"></i> Revert</small></a>');
+                                CTFd.lib.$("#" + String(item.instance_id).substring(0,10) + "_revert_container").html('<a onclick="start_container(\'' + item.docker_image + '\', true);" class=\'btn btn-dark\'><small style=\'color:white;\'><i class="fas fa-redo"></i> Revert</small></a>');
                             }
                         }, 1000);
-                        return true;
-                    };
+                    }
                 });
+
+                return containerFound;
             }).catch(() => {
                 ezal("Attention!", "Error");
+                return false;
             });
         }).catch(() => {
             // TODO
             ezal("Attention!", "Error");
+            return false;
         });
-};
+}
 
 function start_container(container, revert=false) {
-    if (revert === true || get_docker_status(container) !== true) {
-        CTFd.lib.$('#docker_container').html('<div class="text-center"><i class="fas fa-circle-notch fa-spin fa-1x"></i></div>');
-        CTFd.fetch("/api/v1/container?name=" + container )
-            .then((data) => {
-                console.log(data);
-                get_docker_status(container);
-            }).catch(() => {
-                ezal("Attention!", "You can only revert a container once per 5 minutes! Please be patient.");
-                get_docker_status(container);
-            });
-    }
-    
+    get_docker_status(container)
+        .then((docker_status) => {
+            if (revert === true || docker_status !== true) {
+                CTFd.lib.$('#docker_container').html('<div class="text-center"><i class="fas fa-circle-notch fa-spin fa-1x"></i></div>');
+                CTFd.fetch("/api/v1/container?name=" + container)
+                    .then(() => {
+                        get_docker_status(container);
+                    }).catch(() => {
+                        ezal("Attention!", "You can only revert a container once per 5 minutes! Please be patient.");
+                        get_docker_status(container);
+                    });
+            }
+        });
 }
 
 function ezal(title, body) {
